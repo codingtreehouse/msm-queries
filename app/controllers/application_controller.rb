a5945f31aa84ce39bd29e99bd9d65418564ef489
Andrew Johnson <andrewj@chicagobooth.edu>
2023-04-20 15:15:57 +0000

    Build RCAV

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   def homepage
     render({ :template => "misc_templates/home"})
   end
+
+  def actors
+    render({ :template => "layouts/actors.html.erb"})
+  end
+
+  def movies
+    render({ :template => "layouts/movies.html.erb"})
+  end
+
+  def directors
+    render({ :template => "layouts/directors.html.erb"})
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
+  
+  get("/directors", { :controller => "application", :action => "directors" })
+
+  get("/actors", { :controller => "application", :action => "actors" })
+
+  get("/movies", { :controller => "application", :action => "movies" })
 end
