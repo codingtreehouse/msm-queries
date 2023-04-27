67dc120f13830252d08dc772ed57cbaea5ef2e11
Andrew Johnson <andrewj@chicagobooth.edu>
2023-04-27 04:47:03 +0000

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,29 @@
 Rails.application.routes.draw do
+
+  get("/directors" , { :controller => "directors" , :action => "index"}) 
+
+  get("/directors/eldest", { :controller => "directors" , :action => "wisest"})
+
+  get("/directors/youngest", { :controller => "directors" , :action => "youngest"})
+
+  get("/directors/:an_id" , { :controller => "directors" , :action => "directors_details"})
+
+  get("/movies", { :controller => "movies" , :action => "index_movies"})
+
+  get("/movies/:movie_id" , { :controller => "movies" , :action => "movie_details"})
+
+  get("/actors", { :controller => "actors" , :action => "index_actors"})
+
+  get("/actors/:actor_id" , { :controller => "actors" , :action => "actor_details"})
+
   get("/", { :controller => "application", :action => "homepage" })
-  
-  get("/directors", { :controller => "directors", :action => "index" })
 
-  get("/directors/eldest", { :controller => "directors", :action => "wisest" })
 
-  get("/directors/youngest", { :controller => "directors", :action => "youngest" })
 
-  get("/directors/:an_id", { :controller => "directors", :action => "director_details" })
 
-  get("/actors", { :controller => "actors", :action => "index" })
 
-  get("/movies", { :controller => "application", :action => "movies" })
+ 
+
+
+
 end
